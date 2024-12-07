972de290254e93d00384472a25065aaa554a7a5a
António Jacinto <antjacinto1167@gmail.com>
2024-12-07 19:30:31 +0100

    Menu para a pagina carrinho com carrinho, pedido, encomenda

--- app/home/CartScreen.tsx
+++ app/home/CartScreen.tsx
@@ -1,10 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, Touchable, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import IconVectorComponent, { IconType } from '@/components/IconVectorComponent'
+import SearchComponent from '@/components/SearchComponent'
+import CartComponent from '@/components/CartComponentes/CartComponent'
+import OrderComponent from '@/components/CartComponentes/OrderComponent'
+import EncomendaComponent from '@/components/CartComponentes/EncomendaComponent'
+const components:any = [
+  <CartComponent />,
+  <OrderComponent />,
+  <EncomendaComponent />
+]
 
 export default function CartScreen() {
+  const [valueComponent, setValueComponent] = useState<number>(0)
+
   return (
-    <View>
-      <Text>CartScreen</Text>
-    </View>
+    <SafeAreaView>
+      <SearchComponent />
+      <ScrollView>
+
+        <View className=' flex-row  justify-between items-center  border-b border-gray-200 my-4 px-5'>
+          <TouchableOpacity className={`flex-row items-center gap-2   ${valueComponent === 0 ? 'border-b-2 border-[#00665e]' : ''}`} onPress={() => setValueComponent(0)}>
+            <IconVectorComponent icon={{
+              value: {
+                type: IconType.FontAwesomeIcon,
+                name: "shopping-cart"
+              },
+              size: 19,
+              color: valueComponent === 0 ? '#00665e' : 'gray'
+            }} />
+            <Text className={` text-md font-semibold my-2 ${valueComponent === 0 ? 'text-[#00665e]' : 'text-gray-500'}`}>Carrinho</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity className={`flex-row items-center gap-2   ${valueComponent === 1 ? 'border-b-2 border-[#00665e]' : ''}`} onPress={() => setValueComponent(1)}>
+            <IconVectorComponent icon={{
+              value: {
+                type: IconType.EntypoIcon,
+                name: "message"
+              },
+              size: 19,
+              color: valueComponent === 1 ? '#00665e' : 'gray'
+            }} />
+            <Text className={` text-md font-semibold my-2 ${valueComponent === 1 ? 'text-[#00665e]' : 'text-gray-500'}`}>Pedido</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity className={`flex-row items-center gap-2   ${valueComponent === 2 ? 'border-b-2 border-[#00665e]' : ''}`} onPress={() => setValueComponent(2)}>
+            <IconVectorComponent icon={{
+              value: {
+                type: IconType.FontAwesome6Icon,
+                name: "truck"
+              },
+              size: 19,
+              color: valueComponent === 2 ? '#00665e' : 'gray'
+            }} />
+            <Text className={` text-md font-semibold my-2 ${valueComponent === 2 ? 'text-[#00665e]' : 'text-gray-500'}`}>Encomenda</Text>
+          </TouchableOpacity>
+
+        </View>
+
+        <View>
+          <Text>Components</Text>
+          {components[valueComponent]}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -10,7 +10,7 @@ const HomeScreen = () => {
     }
     return (
         <>
-           <Redirect href={"/home/ProductScreen"} />
+           <Redirect href={"/home/CartScreen"} />
         </>
     )
 }

26dc181cfb6561d62907e886c7738aaa96e67888
António Jacinto <antjacinto1167@gmail.com>
2024-12-16 20:25:20 +0100

    Adicionando O primeiro input ao Enviar receita

--- app/home/(homePages)/SendRecipeScreen.tsx
+++ app/home/(homePages)/SendRecipeScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, SafeAreaView, ScrollView, TextInput } from 'react-native'
 import React from 'react'
 import { useNavigation } from 'expo-router'
 import { ArrowLeftIcon } from 'react-native-heroicons/solid'
@@ -12,7 +12,7 @@ export default function SendRecipeScreen() {
     <SafeAreaView>
 
       <View className='bg-[#00665e] p-5 '>
-        <View className='flex-row mt-7 '>
+        <View className='flex-row mt-10 '>
           <TouchableOpacity
             onPress={() => navigation.goBack()}
             className='bg-yellow-400 p-2 -mt-2 rounded-bl-2xl  items-start'
@@ -49,29 +49,47 @@ export default function SendRecipeScreen() {
         <View className='bg-white h-screen'>
           {/* content */}
           <View className='m-5'>
-            <View className='flex-col items-center flex-1'>
-              <Text className='text-2xl font-medium'>Realizar pré-encomenda de receita médica</Text>
-              <Text className='text-md  text-gray-500'>Selecione como pretende submeter a sua receita</Text>
+
+            <View className='flex-col items-center  mb-5'>
+              <Text className='text-lg font-medium'>Realizar pré-encomenda de receita médica</Text>
+              <Text className='text-sm  text-gray-500'>Selecione como pretende submeter a sua receita</Text>
             </View>
 
             <View className='bg-gray-100 p-2 rounded-lg'>
               {/* Buttons */}
               <View className='flex-row justify-around gap-2 items-center'>
-                <TouchableOpacity className='bg-white shadow rounded-lg px-12 py-2 '>
-                  <Text className='font-bold'>SMS</Text>
+                <TouchableOpacity className=' '>
+                  <Text className='font-bold uppercase'>SMS</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity>
+                <TouchableOpacity className=''>
                   <Text>FOTOGRAFIA</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity>
+                <TouchableOpacity className='bg-white shadow rounded-lg px-12 py-2'>
                   <Text>CÓDIGO</Text>
                 </TouchableOpacity>
               </View>
 
+             
+
+
             </View>
+
+             {/* Content first - COnteudo principal */}
+
+             <View>
+                {/* input with label input com label  */}
+                <View>
+                  <Text className='my-4 '>Texto</Text>
+                  <TextInput placeholder='Colar SMS da receita Médica aqui' className='border rounded-md outline-none border-gray-300 placeholder:text-gray-400 p-3' />
+                </View>
+
+              </View>
+
           </View>
+
+
         </View>
 
 
---
+++ components/submitRecipes/MainRecipeButton.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MainRecipeButton = () => {
+  return (
+    <View>
+      <Text>MainRecipeButton</Text>
+    </View>
+  )
+}
+
+export default MainRecipeButton
